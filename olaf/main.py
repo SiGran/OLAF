0e8973b5e225cc226079bb0e20426d794ed0af8a
SiGran <simongrannetia@gmail.com>
2025-04-08 11:59:00 -0600

    updated logic for date selection

--- olaf/main.py
+++ olaf/main.py
@@ -9,27 +9,25 @@ from olaf.processing.graph_data_csv import GraphDataCSV
 from olaf.processing.spaced_temp_csv import SpacedTempCSV
 
 test_folder = (
-    Path.cwd() / "data" / "CAPE_k" / "KCG 5.20.24 base"
+    Path.cwd().parent / "tests" / "test_data" / "test_project" / "SGP 5.15.24 6.20.24 blanks"
 )
-site = "KCG"
-start_time = "2024-05-20 23:36:00"
-end_time = "2024-05-23 03:58:00"
+site = "SGP"
+start_time = "2024-05-15 00:00:00"
+end_time = "2024-05-15 00:00:00"
 filter_color = "white"
-"more adding here?"
-notes = ("Contained 10 um backing. Station not in Baseline. Flows were checked by plugging pumps into regular power strip briefly. "
-         "Then switched back over to Baseline power strip.")
-user = "Carson"
+notes = "PUT THE NOTES HERE"
+user = "JEMOEDER"
 # TODO what to do with number of samples for the blanks
 num_samples = 6  # In the file
-vol_air_filt = 14878.5  # L
+vol_air_filt = 1  # L
 wells_per_sample = 32
 proportion_filter_used = 1.0  # between 0 and 1.0
 vol_susp = 10  # mL
 treatment = (
-     "base",
+    # "base",
     # "heat",
-    #"peroxide",
-    #"blank",
+    # "peroxide",
+    "blank",
     # "blank heat",
     # "blank peroxide,"
 )  # uncomment the one you want to use
@@ -45,26 +43,32 @@ treatment = (
 # }
 
 # Use for side B
-dict_samples_to_dilution = {
-    "Sample_5": 1,
-    "Sample_4": 11,
-    "Sample_3": 121,
-    "Sample_2": 1331,
-    "Sample_1": 14641,
-    "Sample_0": float("inf"),
-}
-
 # dict_samples_to_dilution = {
+#     "Sample_5": 1,
+#     "Sample_4": 11,
+#     "Sample_3": 121,
+#     "Sample_2": 1331,
+#     "Sample_1": 14641,
 #     "Sample_0": float("inf"),
+# }
+
+# Use for Blanks
+# dict_samples_to_dilution = {
+#     "Sample_0" : float("inf"),
 #     "Sample_1": 11,
 #     "Sample_2": 1,
-#}
-# dict_samples_to_dilution = {"Sample_0": float("inf"), "Sample_3": 11, "Sample_4": 1}
+# }
 
+dict_samples_to_dilution = {
+    "Sample_0": float("inf"),
+    "Sample_3": 11,
+    "Sample_4": 1,
+}
 
 header = (
-    f"site = {site}\nstart_time = {start_time}\nend_time = {end_time}\nfilter_color = {filter_color}\n"
-    f"vol_air_filt = {vol_air_filt}\npoprtion_filter_used = {proportion_filter_used}\n"
+    f"site = {site}\nstart_time = {start_time}\nend_time = {end_time}\n"
+    f"filter_color = {filter_color}\n"
+    f"vol_air_filt = {vol_air_filt}\nproportion_filter_used = {proportion_filter_used}\n"
     f"vol_susp = {vol_susp}\ntreatment = {treatment[0]}\nnotes = {notes}\n"
     f"user = {user}\n"
 )
@@ -88,9 +92,9 @@ if __name__ == "__main__":
     # TODO: add check to see if num_samples is same as keys in dict
 
     # GUI
-    # window = tk.Tk()
-    # app = FreezingReviewer(window, test_folder, num_samples, includes=treatment)
-    # window.mainloop()
+    window = tk.Tk()
+    app = FreezingReviewer(window, test_folder, num_samples, includes=treatment)
+    window.mainloop()
 
     # # Processing to create .csv file
     spaced_temp_csv = SpacedTempCSV(test_folder, num_samples, includes=treatment)
@@ -102,10 +106,14 @@ if __name__ == "__main__":
     found_dates = re.findall(DATE_PATTERN, test_folder.name)
     for date in found_dates:
         month, day, year = date.split(".")
-        end_year, end_month, end_day = start_time.split(" ")[0].split("-")
-        if int(end_month) < 10:
-            end_month = end_month[1]
-        if year != end_year[2:] or month != end_month or day != end_day:
+        start_year, str_start_month, start_day = start_time.split(" ")[0].split("-")
+        start_month = int(str_start_month)
+        if start_month < 10:
+            start_month = int(str_start_month[1:])
+            month = int(month[1:])
+        if year != start_year[2:] or str(month) != str(start_month) or day != start_day:
+            print(f"Date {date} does not match with the specified start time: {start_time}")
+            ValueError("Date does not match with the specified start time")
             continue
         # add date to includes
         print(f"Processing data for: {site} {date}")
