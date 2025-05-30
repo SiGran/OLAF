5c1c37442e05a50f80f050cdd80c072528f9f3d2
cbeanshume <carsonchume@gmail.com>
2025-05-30 15:16:06 -0600

    Spelling updates

--- olaf/main_for_blanks.py
+++ olaf/main_for_blanks.py
@@ -4,12 +4,12 @@ from olaf.processing.blank_correction import BlankCorrector
 
 # make decision on how many blanks to use
 # iterate through project folder to find all the blank INPS/L
-# take avg of all
-project_folder = Path.cwd().parent / "tests" / "test_data" / "capek"
+# take average of all blanks
+project_folder = Path.cwd().parent / "data" / "CAPE_k"
 # iterate the project folder to find all the folders with "blanks" and therein all the
 # "frozen_at_temp_reviewed" csv for "blanks"
 
-# Make sure to have an individual "INPS_L_forzen_at_temp..." for each date
+# Make sure to have an individual "INPS_L_frozen_at_temp..." for each date
 corrector = BlankCorrector(project_folder)
 avg_blanks = corrector.average_blanks()
 corrector.apply_blanks()
